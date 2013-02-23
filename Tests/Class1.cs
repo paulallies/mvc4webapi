edc9f676266e084025af08b27f645952bfa98261
Paul Allies <paul.allies@gmail.com>
2013-02-23 23:16:55 +0200

    added web api project

---
+++ Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void test1() {
+            Assert.AreEqual(1, 2);
+        } 
+    }
+}
